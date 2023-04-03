66c50664d8dc3d042aca419d7ed6093a0d3231c8
eusun <eusun0830@gmail.com>
2023-04-03 11:59:27 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tcp_for_typescript
-tcp_for_typescript
+TCP class for typescript
+
+## 1. Install TypeScript
+'''bash
+$ npm install -g typescript
+'''
+
+## 2. Initialize TypeScript Project
+'''bash
+$ tsc --init
+'''
---
+++ tcp.ts
@@ -0,0 +1,125 @@
+// Interfaces
+interface ControlField {
+    URG?: boolean;
+    ACK?: boolean;
+    PSH?: boolean;
+    RST?: boolean;
+    SYN?: boolean;
+    FIN?: boolean;
+}
+
+interface Tcp {
+    sourcePort: number;
+    desticationPost: number;
+    sequenceNumber: number;
+    ackNumber: number;
+    HLEN: number;
+    reserved: number;
+    controlField: ControlField;
+    windowSize: number;
+    checkSum: boolean;
+    urgentPointer: number;
+    options: number;
+}
+
+// interface IP {
+// 	
+// }
+
+
+// Types
+// type SocketType =  Tcp | IP;
+type SocketType = Tcp;
+
+// Socket
+class Socket {
+    private buffer: ArrayBuffer;
+    private view: DataView;
+    private offset: number;
+
+	constructor(options?: SocketType) {
+        this.buffer = new ArrayBuffer(1024);
+        this.view = new DataView(this.buffer);
+        this.offset = 0;
+
+        if (options) {
+            this.writeTcpHeader(options);
+        }
+    }
+
+    writeTcpHeader(header: Tcp) {
+        this.view.setUint16(this.offset, header.sourcePort);
+        this.offset += 2;
+
+        this.view.setUint16(this.offset, header.desticationPost);
+        this.offset += 2;
+
+        this.view.setUint32(this.offset, header.sequenceNumber);
+        this.offset += 4;
+
+        this.view.setUint32(this.offset, header.ackNumber);
+        this.offset += 4;
+
+        const HLEN_RESERVED = ((header.HLEN & 0xf) << 12) | ((header.reserved || 0) & 0x3f);
+        this.view.setUint16(this.offset, HLEN_RESERVED);
+        this.offset += 2;
+
+        const controlField =
+            (header.controlField.FIN ? 1 : 0) |
+            (header.controlField.SYN ? 2 : 0) |
+            (header.controlField.RST ? 4 : 0) |
+            (header.controlField.PSH ? 8 : 0) |
+            (header.controlField.ACK ? 16 : 0) |
+            (header.controlField.URG ? 32 : 0);
+
+        this.view.setUint16(this.offset, controlField);
+        this.offset += 2;
+
+        this.view.setUint16(this.offset, header.windowSize);
+        this.offset += 2;
+
+        this.view.setUint16(this.offset, 0);
+        this.offset += 2;
+
+        this.view.setUint16(this.offset, header.urgentPointer);
+        this.offset += 2;
+    }
+
+	write(buffer: ArrayBuffer, cb?: (err?: Error) => void): boolean {
+        // write the buffer to the socket
+        return true;
+      }
+    
+      connect() {
+        // connect the socket
+      }
+    
+      setEncoding() {
+        // set encoding
+      }
+    
+      pause() {
+        // pause the socket
+      }
+    
+      resume() {
+        // resume the socket
+      }
+    
+      end() {
+        // end the socket connection
+      }
+    
+      emit() {
+        // emit events
+      }
+    
+      on() {
+        // event listener
+      }
+    
+      once() {
+        // register event listener
+      }
+
+}
